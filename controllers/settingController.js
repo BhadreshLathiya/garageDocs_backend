95050487b4cd13321aae0d8c70537663d76334dc
sayog donga <sayogdonga156156@gmail.com>
2023-08-07 21:12:41 +0530

    setting changes

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,4 +1,5 @@
 const Setting = require("../models/settingsModel");
+const User = require("../models/userModel");
 
 exports.addSetting = async (req, res) => {
   try {
@@ -64,11 +65,42 @@ exports.deleteSetting = async (req, res) => {
 
 exports.getSetting = async (req, res) => {
   try {
-    const data= await Setting.find({});
+    const data = await Setting.find({});
     res.status(200).send({
       success: true,
       message: "setting listing successfully",
-      data:data
+      data: data,
+    });
+  } catch (error) {
+    res.status(200).send({
+      success: false,
+      message: "something went wrong",
+    });
+  }
+};
+
+exports.getUserSetting = async (req, res) => {
+  try {
+    const setting = await Setting.findOne({});
+    const user = await User.findById(req.params.id);
+    var eightMonthsFromJan312009 = new Date(
+      new Date(user.createdAt).getFullYear(),
+      new Date(user.createdAt).getMonth(),
+      new Date(user.createdAt).getDate() + Number(setting.trialDays)
+    );
+    var date = new Date(eightMonthsFromJan312009);
+    let today = new Date();
+    if (today > date) {
+      user.isExpire = true;
+      await user.save();
+    } else {
+      user.isExpire = false;
+      await user.save();
+    }
+    res.status(200).send({
+      success: true,
+      message: "user setting listing successfully",
+      data: user,
     });
   } catch (error) {
     res.status(200).send({
