6269accf35fec5ba4661b1ef0c3d9fe517a4733e
sayog donga <sayogdonga156156@gmail.com>
2023-09-24 09:29:01 +0530

    invoice search api changes

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -5,15 +5,7 @@ exports.createInvoice = async (req, res) => {
   try {
     const id = req.params.id;
     const invoiceData = req.body;
-    const invoice = await Invoice.create({
-      mobileNo: invoiceData.mobileNo,
-      name: invoiceData.name,
-      email: invoiceData.email,
-      gstNo: invoiceData.gstNo,
-      address: invoiceData.address,
-      remark: invoiceData.remark,
-      userId: id,
-    });
+    const invoice = await Invoice.create({ ...req.body, userId: id });
     res.status(200).json({
       success: true,
       message: "invoice create successfully.",
